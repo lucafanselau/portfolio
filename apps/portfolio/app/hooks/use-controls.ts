4db7e23fa415d796b4727e207c30453b2eea7b17
Luca Fanselau <luca.fanselau@outlook.com>
2023-03-10 13:58:16 +0100

    🐛 Fix useControls (rules-of-hooks)

--- apps/portfolio/app/hooks/use-controls.ts
+++ apps/portfolio/app/hooks/use-controls.ts
@@ -1,10 +1,9 @@
 import { useControls as useControlsOriginal } from "leva";
 
 // @ts-expect-error This is complete bs, but we want the initial thing to be returned in non dev mode
-export const useControls: typeof useControlsOriginal = (...params) => {
-  if (process.env.NEXT_PUBLIC_NODE_ENV === "development") {
-    return useControlsOriginal(...params);
-  } else {
-    return params[1];
-  }
-};
+export const useControls: typeof useControlsOriginal =
+  process.env.NEXT_PUBLIC_NODE_ENV === "development"
+    ? useControlsOriginal
+    : (...params) => {
+        return params[1];
+      };
