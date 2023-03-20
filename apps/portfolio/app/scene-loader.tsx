c9a2b70e4b071317669c27099f518bb49deec04f
Luca Fanselau <luca.fanselau@outlook.com>
2023-03-20 19:25:44 +0100

    Dynamic loading of the scene

--- apps/portfolio/app/scene-loader.tsx
+++ apps/portfolio/app/scene-loader.tsx
@@ -5,7 +5,9 @@ import { FC, Suspense } from "react";
 
 const SceneComponent = dynamic(() => import("@3d/scene"), {
   ssr: true,
+  loading: () => <div>Loading...</div>,
 });
+
 export const SceneLoader: FC = () => {
   return (
     <Suspense fallback={<p>Loading</p>}>
