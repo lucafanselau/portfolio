d0cf2f02acde0f88a90b18d242f26b2371283c51
Luca Fanselau <lfanselau@tesla.com>
2023-06-20 15:03:48 +0200

    Move over cv content

--- apps/portfolio/content/academic.ts
+++ apps/portfolio/content/academic.ts
@@ -1,1 +1,22 @@
-export const academic = [{}];
+const grade = (grade: number, min: number, max: number, quantile?: number) => ({
+  grade,
+  min,
+  max,
+  quantile,
+});
+
+export const academic = [
+  {
+    institution: "Erzbischöfliche Liebfrauenschule",
+    degree: "Abitur",
+    grade: grade(1.3, 1.0, 4.0),
+    date: [new Date(2011, 6), new Date(2019, 6)],
+  },
+  {
+    institution: "University of Cologne",
+    degree: "B.Sc. Business Informatics",
+    grade: grade(1.2, 1.0, 4.0, 0.025),
+    date: [new Date(2019, 9), new Date(2022, 7)],
+    note: "Honorable mention - Graduated with best possible grade in bachelor thesis in collaboration with onpreo GmBH in applied machine learning topic (real estate appraisal)",
+  },
+];
---
+++ apps/portfolio/content/work.ts
@@ -0,0 +1,36 @@
+export const work = [
+  {
+    company: "Tesla, Inc",
+    motto: "Accelerating the world's transition to sustainable energy",
+    position: "Automation Engineer Internship",
+    link: "https://www.tesla.com/",
+    date: [new Date(2023, 3), new Date(2023, 8)],
+    bullets: [
+      "Internship as Automation Engineer in Giga Factory Berlin / Brandenburg",
+      "Working on internal tooling to support large scale Drive Unit Fabrication",
+    ],
+  },
+  {
+    company: "onpreo GmbH",
+    motto: "Modern acquisition solution for real estate brokers",
+    position: "Working Student - Full Stack Software Engineer",
+    link: "https://www.onpreo.com/",
+    date: [new Date(2022, 0), new Date(2023, 2)],
+    bullets: [
+      "Reviewed and Published Code Changes for Production Environment and Setup of Deployment and Database Infrastructure",
+      "Implemented Integrations to large Real Estate broker platforms by reverse engineering proprietary data exchange formats",
+    ],
+  },
+  {
+    company: "smashleads™",
+    motto: "Intelligent forms & funnels for lead generation",
+    position: "Working Student - Full Stack Software Engineer",
+    link: "https://smashleads.de/",
+    date: [new Date(2019, 10), new Date(2021, 11)],
+    bullets: [
+      "Worked as Full-Stack Developer using a modern Web Development Stack (MongoDB, React, Express, Next.js, Redux, Material UI) in an fast paced Startup Environment",
+      "Responsible for the Conception, Design, Implementation and Deployment of complex features such as Statistics Dashboard Drag and Drop Landingpage Builder or interactive SVG graphics",
+      "Implemented E2E, Integration and Unit Tests for the Code Base and refactored large parts into a modern monorepo design",
+    ],
+  },
+];
